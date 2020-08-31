443f3e2874e5fa1fa7df846fc82680fc60c55051
JarniNaudts <3101Jarni@gmail.com>
2020-08-31 12:56:09 +0200

    added geofence -- pls commit correctly

--- Pokedex/View Controllers/PokedexTableViewController.swift
+++ Pokedex/View Controllers/PokedexTableViewController.swift
@@ -7,14 +7,17 @@
 //
 
 import UIKit
+import CoreLocation
 
-class PokedexTableViewController: UITableViewController {
+class PokedexTableViewController: UITableViewController, CLLocationManagerDelegate {
+	
+	let locationManager:CLLocationManager  = CLLocationManager()
 	
 	var pokemon = [Pokemon]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-		
+		setupGeofence()
 		PokedexController.shared.fetchPokedex(){
 			(pokemon) in
 			if let pokemon = pokemon{
@@ -28,6 +31,29 @@ class PokedexTableViewController: UITableViewController {
 			self.pokemon = pokemon
 			self.tableView.reloadData()
 		}
+	}
+	
+	//For testing change location in the simulator to 51,0944878:3,9121914 for entering pallet town and any coordinates at least 100m away for exiting pallet town
+	func setupGeofence(){
+		locationManager.delegate = self
+		locationManager.requestAlwaysAuthorization()
+		let geoFence:CLCircularRegion = CLCircularRegion(center: CLLocationCoordinate2DMake(51.0944878, 3.9121914), radius: 100, identifier: "PalletTown")
+		locationManager.startMonitoring(for: geoFence)
+		
+	}
+	
+	func locationManager(_ manager: CLLocationManager, didEnterRegion region: CLRegion) {
+		let alert = UIAlertController(title: "Welcome", message: "You entered Pallet Town", preferredStyle: UIAlertController.Style.alert)
+		alert.addAction(UIAlertAction(title: "Ok", style: UIAlertAction.Style.default, handler: nil))
+		self.present(alert, animated: true, completion: nil)
+		print("Entered \(region)")
+	}
+	
+	func locationManager(_ manager: CLLocationManager, didExitRegion region: CLRegion) {
+		let alert = UIAlertController(title: "Goodbye", message: "You are leaving Pallet Town", preferredStyle: UIAlertController.Style.alert)
+		alert.addAction(UIAlertAction(title: "Ok", style: UIAlertAction.Style.default, handler: nil))
+		self.present(alert, animated: true, completion: nil)
+		print("Exited \(region)")
 	}
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
